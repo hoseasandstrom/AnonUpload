3049cb09be485e4cda079c9e1148f945ff66d896
hoseasandstrom <Lakyn4ever>
2016-06-27 12:15:40 -0400

    Added js

---
+++ src/main/java/com/example/controllers/AnonFileRestController.java
@@ -0,0 +1,22 @@
+package com.example.controllers;
+
+import com.example.entities.AnonFile;
+import com.example.services.AnonFileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by hoseasandstrom on 6/27/16.
+ */
+@RestController
+public class AnonFileRestController {
+    @Autowired
+    AnonFileRepository files;
+
+    @RequestMapping(path = "/files", method = RequestMethod.GET)
+    public Iterable<AnonFile> getFiles() {
+        return files.findAll();
+    }
+}
---
+++ src/main/java/com/example/services/AnonFileRepository.java
@@ -0,0 +1,10 @@
+package com.example.services;
+
+import com.example.entities.AnonFile;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by hoseasandstrom on 6/27/16.
+ */
+public interface AnonFileRepository extends CrudRepository<AnonFile, Integer> {
+}
