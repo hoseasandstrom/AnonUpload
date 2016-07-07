6a5d5b5e6b65a3daa99ba18fe288e9fb7276d783
hoseasandstrom <Lakyn4ever>
2016-07-07 11:54:53 -0400

    delete files updated

--- src/main/java/com/example/services/AnonFileRepository.java
+++ src/main/java/com/example/services/AnonFileRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.CrudRepository;
  * Created by hoseasandstrom on 6/27/16.
  */
 public interface AnonFileRepository extends CrudRepository<AnonFile, Integer> {
-    public Iterable<AnonFile> findByForever(Boolean Forever);
+    public Iterable<AnonFile> findByForever(Boolean forever);
+    public int countByForever(Boolean forever);
 
 }
